2eae1609d87e1ffc24913f871479267f6ecbca54
AYUSH KUSHWAHA <ayushkushwaha9818@gmail.com>
2024-07-20 10:22:35 +0530

    updated

--- JobSequencingProble.java
+++ JobSequencingProble.java
@@ -11,7 +11,7 @@ import java.util.*;
  *          if(job(deadline)<time){
  *              add jobs in result
  *              time++
- *          }     
+ *          }     ;;;
  *      }
  */
 public class JobSequencingProble {
